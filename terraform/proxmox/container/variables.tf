94ba977f7b83cef22447d5e013392481385dbd51
Julian L <julian.legler@campus.tu-berlin.de>
2020-11-21 23:45:26 +0100

    feat: Use Terraform to create a Virtual Machine on Proxmox

---
+++ terraform/proxmox/container/variables.tf
@@ -0,0 +1,15 @@
+variable "pub_ssh_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "proxmox_api_password" {
+  default = "password"
+}
+
+variable "proxmox_server_port" {
+  default = 8006
+}
+
+variable "proxmox_server_address" {
+  default = "192.168.2.164"
+}
