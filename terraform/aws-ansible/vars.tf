a7f5dbd3ca9f9c4ca44072126f01b6de056f447b
Roland Odorfer <odorfer@campus.tu-berlin.de>
2020-11-30 16:45:02 +0100

    feat: Use Terraform to create a Virtual Machine on AWS in an configur…

--- terraform/aws-ansible/vars.tf
+++ terraform/aws-ansible/vars.tf
@@ -1,34 +1,34 @@
 variable "region" {
-  type = string
+  type        = string
   description = "AWS region for resources to be created"
-  default = "eu-central-1"
-} 
+  default     = "eu-central-1"
+}
 
 variable "ami" {
-  type = string
+  type        = string
   description = "AMI for the instance to be created"
-  default = "ami-0502e817a62226e03" 
+  default     = "ami-0502e817a62226e03"
   # ubuntu 20.04
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "Instance type for the instance to be created"
-  default = "t2.micro"
+  default     = "t2.micro"
   # 1 vCPU 1gb ram
 }
 
 variable "public_key_path" {
   description = "path to public key that will be uploaded to aws"
-  default = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "private_key_path" {
   description = "path to private key, used to access instance"
-  default = "~/.ssh/id_rsa"
+  default     = "~/.ssh/id_rsa"
 }
 
 variable "remote_user" {
   description = "user for ssh connection"
-  default = "ubuntu"
+  default     = "ubuntu"
 }
