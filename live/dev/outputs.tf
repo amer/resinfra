9efd0387e35fd95e568928d03c7c2759eef9a7b5
Amer <amer.jazaerli@gmail.com>
2021-01-01 13:46:45 +0100

    feat: output cnames

--- live/dev/outputs.tf
+++ live/dev/outputs.tf
@@ -2,8 +2,16 @@ output "azure_aks_siteA_cluster_fqdn" {
   value = module.azure_aks_siteA.cluster_fqdn
 }
 
+output "siteA_cluster_cname" {
+  value = module.azure_aks_siteA.cluster_cname
+}
+
 output "azure_aks_siteB_cluster_fqdn" {
   value = module.azure_aks_siteB.cluster_fqdn
+}
+
+output "siteB_cluster_cname" {
+  value = module.azure_aks_siteB.cluster_cname
 }
 
 //output "kube_config" {
