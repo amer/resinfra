6d5dc7aca7c4148dd7a3885407a6ff6201476426
ciklista <nitschke.jan@googlemail.com>
2021-01-11 22:29:14 +0100

    wip

--- terraform/multi-cloud-demo/main.tf
+++ terraform/multi-cloud-demo/main.tf
@@ -1,12 +1,12 @@
 locals {
-  azure_cidr                = cidrsubnet(var.vpc_cidr, 8, 1)    # 10.1.0.0/16
+  azure_cidr                = cidrsubnet(var.vpc_cidr, 8, 1)    # 10.1.0.0/16 (Azure does not allow to add overlapping subnets when creating vpn routes)
   azure_vm_subnet_cidr      = cidrsubnet(var.vpc_cidr, 16, 256) # 10.1.0.0/24
   azure_gateway_subnet_cidr = cidrsubnet(var.vpc_cidr, 16, 257) # 10.1.1.0/24
 
   gcp_cidr           = cidrsubnet(var.vpc_cidr, 8, 2)    # 10.2.0.0/16
   gcp_vm_subnet_cidr = cidrsubnet(var.vpc_cidr, 16, 512) # 10.2.0.0/24
 
-  hetzner_cidr           = cidrsubnet(var.vpc_cidr, 8, 3)    # 10.3.0.0/16
+  hetzner_cidr           = var.vpc_cidr    # 10.0.0.0/8 (Hetzner needs to have all subnets included in the big VPN)
   hetzner_vm_subnet_cidr = cidrsubnet(var.vpc_cidr, 16, 768) # 10.3.0.0/24
 
   path_private_key = "~/.ssh/ri_key"
@@ -19,7 +19,7 @@ module "hetzner" {
   shared_key                 = var.shared_key
   path_private_key           = local.path_private_key
   path_public_key            = local.path_public_key
-  azure_vm_subnet_cidr       = local.azure_gateway_subnet_cidr
+  azure_vm_subnet_cidr       = local.azure_vm_subnet_cidr
   gcp_gateway_ipv4_address   = module.gcp.gcp_gateway_ipv4_address
   azure_gateway_ipv4_address = module.azure.azure_gateway_ipv4_address
   gcp_vm_subnet_cidr         = local.gcp_vm_subnet_cidr
