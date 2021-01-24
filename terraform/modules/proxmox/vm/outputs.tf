07b2b9d4ddfccfa8a9c435fa2325320bd00df459
Julian L <julian.legler@campus.tu-berlin.de>
2021-01-24 15:24:07 +0100

    wip

--- terraform/modules/proxmox/vm/outputs.tf
+++ terraform/modules/proxmox/vm/outputs.tf
@@ -1,6 +1,6 @@
 // Regex to extract IP because server.ipconfig1 looks like: "ip=92.204.185.34/29,gw=92.204.175.162"
 output "proxmox_public_ips" {
-  value = regex("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b", proxmox_vm_qemu.proxmox_vm.ipconfig1)
+  value = regex("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b", proxmox_vm_qemu.proxmox_vm.*.ipconfig1)
 }
 
 output "gateway_ipv4_address" {
@@ -13,7 +13,7 @@ output "gateway_private_ipv4_address" {
 }
 
 output "proxmox_private_ip_addresses"{
-  value = regex("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b", proxmox_vm_qemu.proxmox_vm.ipconfig0)
+  value = regex("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b", proxmox_vm_qemu.proxmox_vm.*.ipconfig0)
 }
 
 output "ansible_strongswan_updated"{
