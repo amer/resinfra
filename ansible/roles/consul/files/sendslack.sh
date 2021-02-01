eee65ae32c965504f2eecd1a76e0c0b9353eba0b
Julian L <julian.legler@campus.tu-berlin.de>
2021-02-01 18:43:44 +0100

    feat: send watch messages to slack channel

--- ansible/roles/consul/files/sendslack.sh
+++ ansible/roles/consul/files/sendslack.sh
@@ -4,7 +4,9 @@
 consul_json=$(cat /dev/stdin)
 
 # parse the json and get the node name and service names of the critical services
-parsed_json=$( echo "$consul_json" | tr '\r\n' ' ' |  jq '.[] |  {node: .Node, service: .ServiceName, service_state: .Status}' )
+parsed_json=$( echo $consul_json | tr '\r\n' ' ' | jq -r '.[] |  {node: .Node, service: .ServiceName, service_state: .Status}' )
+
+echo $parsed_json
 
 # some informational message for the mail body
 message="This is a automatic alert message from a check handler triggered by a consul watcher\n"
@@ -22,3 +24,17 @@ message="${message}Below is a list of the services with critical health state:\n
 # -S smtp-auth-password="52j8i24pPY45N3GJ" \
 # -S ssl-verify=ignore \
 # julian.legler@gmx.de
+
+payload=${message}${parsed_json}
+
+echo $payload
+
+#escaped_payload=$(echo $payload | sed 's/"/\"/g' | sed "s/'/\'/g" )
+escaped_payload=$(echo $payload | sed 's/"/\\"/g; s/}/\}\n/g')
+postdata="{\"text\":\"$escaped_payload\"}"
+echo $postdata
+
+
+#echo "$postdata"
+
+curl -X POST -H 'Content-type: application/json' --data "$postdata" https://hooks.slack.com/services/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
