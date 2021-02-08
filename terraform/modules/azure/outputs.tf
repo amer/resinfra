6d2c6e0a2412c82263f5d72920efa67df4901e4f
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2021-02-08 17:24:44 +0100

    it does work, but only for <1 minute

--- terraform/modules/azure/outputs.tf
+++ terraform/modules/azure/outputs.tf
@@ -1,8 +1,7 @@
 output "azure_gateway_ipv4_address" {
-  value = azurerm_public_ip.gateway.ip_address
+  value = azurerm_public_ip.gateway.*.ip_address
 }
 
 output "azure_private_ip_addresses" {
   value = azurerm_linux_virtual_machine.worker_vm.*.private_ip_address
-
 }
