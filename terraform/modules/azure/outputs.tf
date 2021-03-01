606abe88cca130c4f13b1d0dff2d227af6a83a75
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2021-03-01 17:27:47 +0100

    Fix: re-enable BGP

--- terraform/modules/azure/outputs.tf
+++ terraform/modules/azure/outputs.tf
@@ -2,11 +2,6 @@ output "azure_gateway_ipv4_address" {
   value = azurerm_public_ip.gateway.ip_address
 }
 
-output "azure_non_bgp_gateway_ipv4_address" {
-  value = azurerm_public_ip.gateway-non-bgp.ip_address
-}
-
 output "azure_private_ip_addresses" {
   value = azurerm_linux_virtual_machine.worker_vm.*.private_ip_address
-
 }
